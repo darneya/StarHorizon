365fdad33473d436caf1aeec7a5cbc2ef53a2ee4
EvilBug <Dalexiandr@gmail.com>
2025-03-30 21:10:33 +0300

    fix(lang): русификация имен персонажей

--- Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
+++ Content.Shared/Humanoid/Prototypes/SpeciesPrototype.cs
@@ -87,6 +87,14 @@ public sealed partial class SpeciesPrototype : IPrototype
     [DataField]
     public string FemaleFirstNames { get; private set; } = "names_first_female";
 
+    // Horizon start
+    [DataField]
+    public string MaleLastNames { get; private set; } = "names_last_male";
+
+    [DataField]
+    public string FemaleLastNames { get; private set; } = "names_last_female";
+    // Horizon end
+
     [DataField]
     public string LastNames { get; private set; } = "names_last";
 
