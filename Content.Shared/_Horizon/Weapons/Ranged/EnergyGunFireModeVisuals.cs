f87dc166ab57fab228cc7396d004711a330b1ff5
fospas <fospas1710@gmail.com>
2025-06-12 16:00:54 +0300

    wip: расширен арсенал протокинетического оружия и модулей к нему (без проджект тайлов)

---
+++ Content.Shared/_Horizon/Weapons/Ranged/EnergyGunFireModeVisuals.cs
@@ -0,0 +1,20 @@
+/*
+
+using Robust.Shared.Serialization;
+
+namespace Content.Shared.Aavikko.Weapons.Ranged;
+
+[Serializable, NetSerializable]
+public enum EnergyGunFireModeVisuals : byte
+{
+    State
+}
+
+[Serializable, NetSerializable]
+public enum EnergyGunFireModeState : byte
+{
+    Disabler,
+    Lethal,
+    Special
+}
+*/
