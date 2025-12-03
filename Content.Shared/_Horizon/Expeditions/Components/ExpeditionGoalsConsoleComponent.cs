a9502ee47c5bea949b5b1660af373c195d0934b0
FaDeOkno <logkedr18@gmail.com>
2025-12-03 18:38:54 +0400

    fix: Фикс консоли

--- Content.Shared/_Horizon/Expeditions/Components/ExpeditionGoalsConsoleComponent.cs
+++ Content.Shared/_Horizon/Expeditions/Components/ExpeditionGoalsConsoleComponent.cs
@@ -1,5 +1,8 @@
+using Robust.Shared.GameStates;
+
 namespace Content.Shared._Horizon.Expeditions;
 
+[RegisterComponent, NetworkedComponent]
 public sealed partial class ExpeditionGoalsConsoleComponent : Component
 {
 }
