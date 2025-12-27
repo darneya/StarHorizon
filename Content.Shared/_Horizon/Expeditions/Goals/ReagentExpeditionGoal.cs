8f5c8f4253c008ea30639e186f2f8e56ca505e6d
FaDeOkno <logkedr18@gmail.com>
2025-12-28 03:08:29 +0400

    add: Цель на реагенты

---
+++ Content.Shared/_Horizon/Expeditions/Goals/ReagentExpeditionGoal.cs
@@ -0,0 +1,72 @@
+using Content.Shared.Chemistry.Components.SolutionManager;
+using Content.Shared.Chemistry.EntitySystems;
+using Content.Shared.Destructible.Thresholds;
+using Content.Shared.Stacks;
+using Content.Shared.Storage.Components;
+using Content.Shared.Storage.EntitySystems;
+using Content.Shared.Tag;
+using Robust.Shared.Random;
+using Robust.Shared.Serialization;
+
+namespace Content.Shared._Horizon.Expeditions;
+
+[Serializable, NetSerializable]
+public sealed partial class ReagentExpeditionGoal : ExpeditionGoal
+{
+    [DataField(required: true), ViewVariables(VVAccess.ReadWrite)]
+    public string RequiredReagent = default!;
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public int RequiredAmount;
+
+    public override ExpeditionGoal Instantiate(int amount)
+    {
+        return new ReagentExpeditionGoal()
+        {
+            Description = Loc.GetString(Description, ("amount", amount)),
+            IconEntity = IconEntity,
+            Reward = Reward,
+            RequiredReagent = RequiredReagent,
+            RequiredAmount = amount
+        };
+    }
+
+    public override bool TryComplete(EntityUid sellEntity, IEntityManager entMan)
+    {
+        int count = 0;
+
+        IncreaseFromStack(sellEntity, ref count, entMan);
+
+        var entStorage = entMan.System<SharedEntityStorageSystem>();
+        SharedEntityStorageComponent? storage = null;
+        entStorage.ResolveStorage(sellEntity, ref storage);
+
+        if (storage != null)
+        {
+            foreach (var item in storage.Contents.ContainedEntities)
+                IncreaseFromStack(item, ref count, entMan);
+        }
+
+        return count >= RequiredAmount;
+    }
+
+    private void IncreaseFromStack(EntityUid sellEntity, ref int count, IEntityManager entMan)
+    {
+        var solutionContainer = entMan.System<SharedSolutionContainerSystem>();
+
+        if (!entMan.TryGetComponent<SolutionContainerManagerComponent>(sellEntity, out var solMan))
+            return;
+
+        foreach (var item in solMan.Containers)
+        {
+            if (!solutionContainer.TryGetSolution(solMan, item, out var solution))
+                continue;
+
+            foreach (var reagent in solution.Contents)
+            {
+                if (reagent.Reagent.Prototype == RequiredReagent)
+                    count += reagent.Quantity.Int();
+            }
+        }
+    }
+}
