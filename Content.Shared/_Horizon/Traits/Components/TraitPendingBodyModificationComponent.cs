6041c7c35866ef52545e78bfd656d6304591ab2f
FaDeOkno <logkedr18@gmail.com>
2026-02-03 13:14:12 +0400

    add: Трейты модифицирующие тело

---
+++ Content.Shared/_Horizon/Traits/Components/TraitPendingBodyModificationComponent.cs
@@ -0,0 +1,42 @@
+using Content.Shared.Body.Part;
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Horizon.Traits;
+
+[RegisterComponent]
+public sealed partial class TraitPendingBodyModificationComponent : Component
+{
+    [ViewVariables(VVAccess.ReadWrite)]
+    public List<PartReplacement> Parts = new();
+
+    [ViewVariables(VVAccess.ReadWrite)]
+    public List<OrganReplacement> Organs = new();
+}
+
+public sealed class PartReplacement
+{
+    public BodyPartType PartType;
+    public BodyPartSymmetry Symmetry;
+    public string? ProtoId = null;
+    public string? SlotId = null;
+
+    public PartReplacement(BodyPartType partType, BodyPartSymmetry symmetry, string? protoId = null, string? slotId = null)
+    {
+        PartType = partType;
+        Symmetry = symmetry;
+        ProtoId = protoId;
+        SlotId = slotId;
+    }
+};
+
+public sealed class OrganReplacement
+{
+    public string OrganProto;
+    public string OrganSlot;
+
+    public OrganReplacement(string organSlot, string organProto)
+    {
+        OrganSlot = organSlot;
+        OrganProto = organProto;
+    }
+}
