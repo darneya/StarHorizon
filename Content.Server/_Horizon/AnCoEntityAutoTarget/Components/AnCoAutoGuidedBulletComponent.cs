aa9ed8cddc043b8a49c4c4e5fe923019d2690e28
Dismerty <dismerty@gmail.com>
2026-02-20 19:45:55 +0700

    add: Добавлена система автонаведения, компонент автонаведения пули и компонент настройки пуль для оружия.

---
+++ Content.Server/_Horizon/AnCoEntityAutoTarget/Components/AnCoAutoGuidedBulletComponent.cs
@@ -0,0 +1,23 @@
+using Content.Shared.Whitelist;
+
+namespace Content.Server._Horizon.AnCoAutoGuidedBullet.Components;
+
+/// <summary>
+/// Компонент который добавляется пулям для автоматической наводки на цель.
+/// Если сущность попадает в радиус действия пули, пуля меняет траекторию.
+/// </summary>
+[RegisterComponent]
+public sealed partial class AnCoAutoGuidedBulletComponent : Component
+{
+    [DataField]
+    public float Range;
+
+    [DataField]
+    public float TurnSpeed;
+
+    [DataField]
+    public EntityUid Shooter;
+
+    [DataField]
+    public EntityUid? Target;
+}
