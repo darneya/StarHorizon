b11981fb6aee305cff3797141552de8051312f14
darneya <darneya.git>
2026-07-11 14:09:41 +0300

    EmpImmune + батарейки

---
+++ Content.Shared/Emp/EmpImmuneComponent.cs
@@ -0,0 +1,11 @@
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._rage.Emp;
+
+/// <summary>
+/// Даёт полный иммунитет к ЭМИ-воздействиям.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class EmpImmuneComponent : Component
+{
+}
